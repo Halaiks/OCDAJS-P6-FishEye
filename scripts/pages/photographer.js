998ee2b90aa3560e0838c9cf9e1f829bd71202b5
Alexandre GUILLAUME <AlexGuillaume94@gmail.com>
2024-10-03 10:56:46 +0200

    Réutilisation de getPhotographers et photographerTemplate

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,53 +1,39 @@
-// Fonction pour récupérer l'ID du photographe depuis l'URL
+async function getPhotographers() {
+    const response = await fetch('data/photographers.json');
+    const data = await response.json();
+    return { photographers: data.photographers };
+}
+
 function getPhotographerIdFromUrl() {
     const params = new URLSearchParams(window.location.search);
-    return params.get('id'); // Récupère l'ID du photographe dans l'URL
+    return params.get('id');
 }
 
-// Fonction pour récupérer les données du photographe correspondant à l'ID
 async function getPhotographerData() {
-    const photographerId = getPhotographerIdFromUrl(); // Appel de la fonction pour obtenir l'ID
-    if (!photographerId) {
-        console.error("Aucun ID de photographe trouvé dans l'URL");
-        return;
-    }
-    
-    // Récupérer les données des photographes à partir du fichier JSON
-    try {
-        const response = await fetch('data/photographers.json');
-        const data = await response.json();
-        const photographer = data.photographers.find(p => p.id == photographerId);
+    const photographerId = getPhotographerIdFromUrl();
+    const { photographers } = await getPhotographers();
+    const photographer = photographers.find(p => p.id == photographerId);
 
-        if (photographer) {
-            console.log("Photographe sélectionné :", photographer); // Vérification console
-            displayPhotographer(photographer); // Appel de la fonction pour afficher les données
-        } else {
-            console.error("Photographe non trouvé");
-        }
-    } catch (error) {
-        console.error("Erreur lors de la récupération des données :", error);
+    if (photographer) {
+        displayPhotographer(photographer);
     }
 }
 
-// Fonction pour afficher les données du photographe dans la section .photograph-header
 function displayPhotographer(photographer) {
-    // Remplir les informations sur la page
     const photographHeader = document.querySelector('.photograph-header');
-    
-    // Injecter le contenu dynamique
+    const photographerModel = photographerTemplate(photographer);
+
     photographHeader.innerHTML = `
-        <div class="photograph-info">
-            <h1>${photographer.name}</h1>
-            <p>${photographer.city}, ${photographer.country}</p>
-            <p>${photographer.tagline}</p>
-        </div>
-        <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
-        <div class="photograph-portrait">
-            <img src="assets/photographers/${photographer.portrait}" alt="Portrait de ${photographer.name}">
-        </div>
-        
+            <div class="photograph-info">
+            <h2>${photographerModel.name}</h2>
+            <p class="location">${photographer.city}, ${photographer.country}</p>
+            <p class="tagline">${photographer.tagline}</p>
+            </div>
+            <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+            <div class="photograph-portrait">
+            <img src="${photographerModel.picture}">
+            </div>
     `;
 }
 
-// Appel de la fonction pour obtenir et afficher les données du photographe
 getPhotographerData();
