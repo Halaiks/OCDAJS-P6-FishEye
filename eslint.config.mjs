18b7c3bb606ff6f061740787d2abb68a47b37d91
Alexandre GUILLAUME <AlexGuillaume94@gmail.com>
2025-01-08 13:28:08 +0100

    Maj EsLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    settings: {
-      react: {
-        version: "detect", // Utilise automatiquement la version de React installée
-      },
-    },
-  },
 ];
